12233054d39aae0ab66c682a6c7324ea7fb527b0
nandips2 <siri.nandipaty2@mail.dcu.ie>
2024-01-31 12:44:39 +0000

    added functions for user auth

---
+++ TerraTalk/app/src/main/java/com/example/terratalk/LoginRegister/Login.kt
@@ -0,0 +1,54 @@
+package com.example.terratalk.LoginRegister
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+
+
+fun loginUser(email: String, password: String) {
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Login successful
+                val user = auth.currentUser
+            } else {
+                // Login failed
+                Log.w("Login", "signInWithEmail:failure", task.exception)
+            }
+        }
+}
+
+fun registerUser(email: String, password: String) {
+    auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Registration successful
+                val user = auth.currentUser
+            } else {
+                // Registration failed
+                Log.w("Registration", "createUserWithEmail:failure", task.exception)
+            }
+        }
+}
+
+fun logOut(){
+    auth.signOut()
+}
+
+fun sendEmailVerification(context: Context) {
+    auth.currentUser?.sendEmailVerification()
+        ?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(context, "Verification email sent", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e("Verification", "sendEmailVerification:failure", task.exception)
+                Toast.makeText(context, "Failed to send verification email", Toast.LENGTH_SHORT).show()
+            }}}
+
--- TerraTalk/app/src/main/java/com/example/terratalk/MainActivity.kt
+++ TerraTalk/app/src/main/java/com/example/terratalk/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.terratalk
 import com.example.terratalk.Webscrapping.parseIrishTimes
 import com.example.terratalk.Webscrapping.parseIndependent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.terratalk.ui.theme.TerraTalkTheme
@@ -10,8 +11,10 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.terratalk.Webscrapping.NewsPage
 import com.example.terratalk.Webscrapping.NewsViewModel
 import com.example.terratalk.Webscrapping.parseTheJournal
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import com.google.firebase.auth.FirebaseUser
 
 
 
@@ -22,7 +25,17 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+       val myAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            // User is already logged in
+            Log.d("Authentication", "User is already logged in. Email: ${currentUser.email}")
+            // Perform any additional actions if needed
+        } else {
+            Log.d("Authentication", "User is not logged in")
+        }
 
         MainScope().launch {
             val newsItems1 = parseIrishTimes()
